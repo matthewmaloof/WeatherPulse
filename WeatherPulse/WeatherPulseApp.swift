abb8730286910d42ed2c45fdef38d4f6a842e831
mattmaloof <mattmaloof@gmail.com>
2023-10-23 20:59:56 -0400

    Initial Commit for WeatherPulse

---
+++ WeatherPulse/WeatherPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPulseApp.swift
+//  WeatherPulse
+//
+//  Created by Matthew Maloof on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
